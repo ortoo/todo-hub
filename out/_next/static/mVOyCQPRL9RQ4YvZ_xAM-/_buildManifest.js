2349fa6b356655a632c4692775b547042b1152c4
student <student@James-MacBook-Pro.local>
2020-12-14 00:02:38 +0000

    Deploy gh-pages

---
+++ out/_next/static/mVOyCQPRL9RQ4YvZ_xAM-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d14606f878741832061f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a680f3f123f490c82106.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
