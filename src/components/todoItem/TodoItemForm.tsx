25d8e15461bd240f6522ac2ef44e77e8a94887dc
Kyle Selman <kyle@kyleselman.co.uk>
2022-01-18 11:24:45 +0000

    fix(main): fix slight visual bug in form

--- src/components/todoItem/TodoItemForm.tsx
+++ src/components/todoItem/TodoItemForm.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import { Theme } from "@mui/material/styles";
 
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 
 import { TodoItem } from "./TodoItem";
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
     titleField: {
+      marginTop: theme.spacing(1),
       marginBottom: theme.spacing(2),
     },
   };
@@ -26,29 +27,31 @@ const TodoItemForm = ({ todoItem, onChange }: Props) => {
 
   const { title, description } = todoItem;
 
-  return <>
-    <TextField
-      className={classes.titleField}
-      fullWidth
-      variant="outlined"
-      label="Title"
-      value={title}
-      onChange={(event) => {
-        onChange({ ...todoItem, title: event.target.value });
-      }}
-    />
-    <TextField
-      fullWidth
-      variant="outlined"
-      label="Description"
-      multiline
-      maxRows={4}
-      value={description}
-      onChange={(event) => {
-        onChange({ ...todoItem, description: event.target.value });
-      }}
-    />
-  </>;
+  return (
+    <>
+      <TextField
+        className={classes.titleField}
+        fullWidth
+        variant="outlined"
+        label="Title"
+        value={title}
+        onChange={(event) => {
+          onChange({ ...todoItem, title: event.target.value });
+        }}
+      />
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Description"
+        multiline
+        maxRows={4}
+        value={description}
+        onChange={(event) => {
+          onChange({ ...todoItem, description: event.target.value });
+        }}
+      />
+    </>
+  );
 };
 
 export default TodoItemForm;
